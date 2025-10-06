8b1f568ff46b537d98049c079b07543e8181c1f2
simonpcouch <simonpatrickcouch@gmail.com>
2025-10-06 09:32:14 -0500

    only assemble message on failure (#159)

--- tests/testthat/helper-valid_log.R
+++ tests/testthat/helper-valid_log.R
@@ -70,18 +70,21 @@ expect_valid_log <- local({
 
     status <- attr(result, "status")
 
-    # "inst/test/inspect/logs/2025-03-24T10-39-36-05-00_simple-arithmetic_fQ9mYnqZFhtEuUenPpJgKL.json"
-    if (length(result) == 1) {
-      formatted_message <- cli::format_message(c(
-        "The generated log did not pass the pydantic model:",
-        glue::glue("{{.field {result[1]}}}")
-      ))
-      expect(
-        is.null(status) || status == 0,
-        formatted_message
-      )
-    }
+    expect(
+      is.null(status) || status == 0,
+      formatted_pydantic_error(result)
+    )
+  }
+})
 
+formatted_pydantic_error <- function(result) {
+  # "inst/test/inspect/logs/2025-03-24T10-39-36-05-00_simple-arithmetic_fQ9mYnqZFhtEuUenPpJgKL.json"
+  if (length(result) == 1) {
+    formatted_message <- cli::format_message(c(
+      "The generated log did not pass the pydantic model:",
+      glue::glue("{{.field {result[1]}}}")
+    ))
+  } else {
     # Make the result more readable by removing redundant elements
     # and formatting indices with cli (#159)
     result <- result[!grepl("For further information visit", result)]
@@ -102,10 +105,5 @@ expect_valid_log <- local({
       "",
       result_with_breaks
     ))
-
-    expect(
-      is.null(status) || status == 0,
-      formatted_message
-    )
   }
-})
+}
